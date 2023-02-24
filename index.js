e09d5469be2b2ff50dde1b1a4f0720588c622dcc
Josh Haawke <joshuathawke@gmail.com>
2023-02-24 09:14:49 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+//saturdayFun("bathe my dog");
+
+function mondayWork(value){
+    if (value == undefined){
+        return "This Monday, I will go to the office."
+    } else {
+        return `This Monday, I will ${value}.`
+    }
+}
+
+function wrapAdjective(value){
+    return function (emphatic){
+        return `You are ${value}${emphatic}${value}!`
+    }
+}
